20d2516c6c4468cb961a061338238140d3f2043a
Brody <947431990@qq.com>
2022-08-08 21:33:48 +0800

    LeetCode33题，看到时间复杂度为log，说明要往二分法上面靠拢

--- leet-code/src/main/java/org/brody/leetcode/SearchInRotatedSortedArray.java
+++ leet-code/src/main/java/org/brody/leetcode/SearchInRotatedSortedArray.java
@@ -2,21 +2,23 @@ package org.brody.leetcode;
 
 /**
  * 33. 搜索旋转排序数组
+ * <p>
  * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
  * <p>
- * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * ( 例如，数组[0,1,2,4,5,6,7]可能变为[4,5,6,7,0,1,2])。
  * <p>
- * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回-1。
  * <p>
  * 你可以假设数组中不存在重复的元素。
  * <p>
- * 你的算法时间复杂度必须是 O(log n) 级别。
+ * 你的算法时间复杂度必须是O(logn) 级别。
  * <p>
  * 示例 1:
  * <p>
  * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * <p>
  * 输出: 4
- * 示例 2:
+ * 示例2:
  * <p>
  * 输入: nums = [4,5,6,7,0,1,2], target = 3
  * 输出: -1
@@ -27,11 +29,19 @@ package org.brody.leetcode;
  *
  * @author CYF
  */
-public class $0033_SearchInRotatedSortedArray {
+public class SearchInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        SearchInRotatedSortedArray searchInRotatedSortedArray = new SearchInRotatedSortedArray();
+        System.out.println(searchInRotatedSortedArray.search(nums, target));
+    }
+
     public int search(int[] nums, int target) {
-        //旋转之后的数组可以分为两类，
+        // 旋转之后的数组可以分为两类，
         // 一类是nums[start] <= nums[mid]，此时前半部分有序。因此如果 nums[start] <= target < nums[mid]。则在前半部分找，否则去后半部分找
-        //一类是nums[start] > nums[mid]，此时后半部分有序。如果nums[mid] <target <= nums[end]。在后半部分找，否则去前半部分找
+        // 一类是nums[start] > nums[mid]，此时后半部分有序。如果nums[mid] <target <= nums[end]。在后半部分找，否则去前半部分找
         if (nums == null || nums.length == 0) {
             return -1;
         }
@@ -43,9 +53,9 @@ public class $0033_SearchInRotatedSortedArray {
             if (nums[mid] == target) {
                 return mid;
             }
-            //前半部分有序，注意此处用小于等于
+            // 前半部分有序，注意此处用小于等于，因为要考虑到nums长度为偶数
             if (nums[start] <= nums[mid]) {
-                //target在前半部分
+                // target在前半部分
                 if (nums[start] <= target && target < nums[mid]) {
                     end = mid - 1;
                 } else {
